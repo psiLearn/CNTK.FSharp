ef456eaed8a3002871e59de41676359c0deb265d
PsiLearn <zu.mir@gmx.de>
2018-02-15 11:27:17 +0100

    completed loading dat into memory, updatete gitignore, still missing saving text adn xml

--- examples/Cifar/installCifar.fsx
+++ examples/Cifar/installCifar.fsx
@@ -1,3 +1,4 @@
+open System.Runtime.InteropServices
 #I "../../packages/FSharp.Data/lib/net45"
 #I "../../packages/SharpZipLib/lib/20"
 #r "FSharp.Data.dll"
@@ -6,9 +7,10 @@
 
 [<AutoOpen>]
 module cifarUtil = 
-    open FSharp.Data
     open System.IO
+    open System.Drawing
     open System.IO.Compression
+    open FSharp.Data
     open ICSharpCode.SharpZipLib.Tar
 
     /// Combine paths
@@ -42,19 +44,23 @@ module cifarUtil =
         use tar = TarArchive.CreateInputTarArchive(fs )
         tar.ExtractContents dirName
         dirName
- 
-    let readBatch src = 
+
+    /// Reads the information of a 
+    let readBatch dims src = 
         use fs = new FileStream(src,FileMode.Open)
         use reader = new BinaryReader(fs)
         let rec readPictures pictures = 
             if fs.Position < fs.Length then 
                 let feature = reader.ReadByte()
-                let picture = reader.ReadBytes(32*32*3)
+                let picture = reader.ReadBytes(dims |> List.reduce (*))
                 (feature,picture)::pictures |> readPictures 
             else pictures   
         readPictures []                 
 
-    let loadData src =
+    let getLabelNames src =
+        File.ReadAllLines <| src @@ "batches.meta.txt"
+
+    let loadData dims src =
         let files = Directory.GetFiles(src,"*.bin")
         let groups = 
             files
@@ -67,32 +73,68 @@ module cifarUtil =
         let foldGroup gr= 
             gr 
             |> Array.fold (fun acc f-> 
-                readBatch f |> List.append acc
+                readBatch dims f |> List.append acc
                  )  []    
-        foldGroup groups.[0], foldGroup groups.[1]
+        foldGroup groups.[0], foldGroup groups.[1], getLabelNames src
 
+        
+    let saveAsPNG fName (im:byte [,,]) (iDim, jDim) = 
+        let bArray = Array.zeroCreate (iDim*jDim*3)
+        bArray
+        |> Array.iteri (fun i _ ->
+            let x = i/(3*iDim)
+            let y = ( i%(3*iDim) ) / 3
+            let c = i%3
+            bArray.[i] <- byte im.[c,x,y])
+        use bmp = new Bitmap(iDim, jDim, Imaging.PixelFormat.Format24bppRgb)        
+        let bmpData = 
+            bmp.LockBits(   Rectangle(0, 0,bmp.Width, bmp.Height),
+                            Imaging.ImageLockMode.WriteOnly,
+                            bmp.PixelFormat);
 
-    let  saveTrainImages (iDim,jDim)  (data: (byte * byte[]) list) foldername =
+        let pNative = bmpData.Scan0;
+        Marshal.Copy(bArray, 0, pNative, bArray.Length);
+
+        bmp.UnlockBits(bmpData);
+
+        bmp.Save(fName);
+    
+    type SaveFunction =  (int*int) -> string -> int -> byte[,,] -> unit
+    let saveImageFunction (iDim,jDim) foldername iFile imData= 
+        let fName = Path.Combine (foldername, sprintf "%05d.png" iFile )
+        printfn "saving %s" fName
+        saveAsPNG fName imData (iDim,jDim)
+           
+
+    let  saveTrainImages (fo:SaveFunction option) (iDim,jDim)  (data: (byte * byte[]) list) foldername =
         if  Directory.Exists(foldername) |> not then 
             Directory.CreateDirectory foldername |> ignore
         data 
         |> List.indexed
         |> List.fold (fun (acc:int[,,]) (iFile,(_label, im)) ->
-            let fName = Path.Combine (foldername, sprintf "%05d.png" iFile )
-            printfn "not saving %s" fName
-            //todo ad save png here
+            let imData = Array3D.zeroCreate 3 iDim jDim
             im 
             |> Array.splitInto (3)
             |> Array.iteri (fun i ca -> 
                 ca |> Array.chunkBySize (iDim)
                 |> Array.iteri (fun j cv -> 
                     cv |> Array.iteri (fun k b -> 
+                        imData.[i,j,k] <-   b
                         acc.[i,j,k] <- acc.[i,j,k] + (int b))
-                    )
-            )
+            ))
+            fo |> Option.iter (fun f ->
+                f (iDim,jDim) foldername iFile imData)
             acc
         )   (Array3D.init 3 iDim jDim (fun _ _ _ -> 0))
         |> Array3D.map (fun  v -> v / data.Length) 
+    let getLabels:('a * 'b)list -> (int * 'a) list  = List.map fst >> List.indexed 
+    let letGetMeanAndSave (width,height) folder  data=     
+        let dirPath =  __SOURCE_DIRECTORY__ @@ folder
+        let saveFunctionOption = 
+            match System.IO.Directory.Exists <| dirPath with
+            | false -> Some saveImageFunction
+            | true -> None
+        saveTrainImages saveFunctionOption (width, height) data dirPath
 
 /// URL of the binary dataset
 let dataSetUrl = "http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz"
@@ -100,17 +142,37 @@ let dataSetUrl = "http://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz"
 /// File name of the downloaded data
 let dataFileName = __SOURCE_DIRECTORY__ + "/cifar-10-binary.tar.gz"
 
-download dataSetUrl dataFileName
-decompress dataFileName
-|> untar 
-let (trn,test) = 
-    @"C:\Users\siebkpte\fsharp\Cntk\CNTK.FSharp\examples\Cifar\cifar-10-binary\cifar-10-batches-bin"
-    |> loadData
+// Dimensions of the data:
+//      Number of colors, width of the picture, height of the picture
+let nColors = 3;
+let width = 32;
+let height = 32;
 
-// 
-//     @"C:\Users\siebkpte\fsharp\Cntk\CNTK.FSharp\examples\Cifar\cifar-10-binary\cifar-10-batches-bin\data_batch_1.bin"
-//     |> readBatch
+// Downloading the data
+if  __SOURCE_DIRECTORY__ @@ "test" 
+    |> System.IO.Directory.Exists
+    |> not then
+                download dataSetUrl dataFileName
+// get the data from the tar archive
+                decompress dataFileName
+                |> untar 
+                |> ignore
+
+// get the data  into memory
+let (trn,test,LabelNames) = 
+    @"C:\Users\siebkpte\fsharp\Cntk\CNTK.FSharp\examples\Cifar\cifar-10-binary\cifar-10-batches-bin"
+    |> loadData [nColors; width; height]
+
+// getting the labels of the training data
+let trnLabels = trn |> getLabels
+let testLabels = test |> getLabels
+
+// save the images and get the mean data
+
+// only saving if there is no train directory
+let dataMeanTraining =     
+    letGetMeanAndSave (width,height) "train" trn
 let dataMeanTest =     
-    saveTrainImages (32, 32) test (__SOURCE_DIRECTORY__ @@ "Test")
+    letGetMeanAndSave (width,height) "test" test
 
 dataMeanTest.[2,0,0]
